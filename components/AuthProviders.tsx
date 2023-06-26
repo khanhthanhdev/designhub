526f195a5941d313dbfd494a69df16c53dde515c
khanhthanhdev <thanhkt27507@gmail.com>
2023-06-26 16:02:44 +0700

    edit, delete project

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -2,6 +2,7 @@
 
 import {useState, useEffect} from 'react';
 import React, {getProviders, signIn} from 'next-auth/react';
+import Button from './Button';
 
 type Provider = {
     id: string;
@@ -33,7 +34,11 @@ const AuthProviders = () => {
     return (
         <div>
             {Object.values(providers).map((provider: Provider, i) => (
-                <button key={i} onClick={() => signIn(provider?.id)}>{provider.id}</button>
+                <Button 
+                key={i}
+                 handleClick={() => signIn(provider?.id)}
+                 title="Sign In"
+                 />
             ))}
         </div>
     )
